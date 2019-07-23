94135c9ee2da2c2bcfa3720e4119c0c678d5a47c
adikkharat <aditya.kharat123@gmail.com>
2019-07-23 12:39:34 +0530

    Jenkins Again

--- JenkinsTest.java
+++ JenkinsTest.java
@@ -1,7 +1,9 @@
-Class Hello
-{
-public static void main(String args[])
-{
-System.out.Println("Hello World");
-}
-}
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		 //TODO Auto-generated method stub
+		 		System.out.println("Hello World");
+		
+		 }
+		
+	}
